8f304cbbeed17d04576fd74cee24b76d07d474d8
Marcos Jose de Jesus <using.rpa.projects@outlook.com>
2020-06-08 01:40:26 -0300

    Carregamento de cidades

---
+++ ClientApp/src/app/cities/cities.component.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//@Component({
+//  selector: 'app-cities',
+//  templateUrl: './cities.component.html',
+//  styleUrls: ['./cities.component.css']
+//})
+//export class CitiesComponent {
+//  public cities: City[];
+//  constructor(
+//    private http: HttpClient,
+//    @Inject('BASE_URL') private baseUrl: string) {
+//  }
+//  ngOnInit() {
+//    http.get<City[]>(baseUrl + 'api/Cities')
+//      .subscribe(result => {
+//        this.cities = result;
+//      }, error => console.error(error))
+//  }
+//}
+//# sourceMappingURL=cities.component.js.map
