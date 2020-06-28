e32cf69cf5f045410c23db2822937632cd952b5b
Marcos Jose de Jesus <using.rpa.projects@outlook.com>
2020-06-28 01:40:07 -0300

    Criação te poup-up para criar nova Cidade

--- ClientApp/src/app/cities/city-edit.component.ts
+++ ClientApp/src/app/cities/city-edit.component.ts
@@ -17,8 +17,13 @@ export class CityEditComponent {
   // o modelo do formulário
   form: FormGroup;
 
-  // objeto City para edição
+  // objeto City para edição ou criação
   city: City;
+
+  // o ID do objeto da city, tera o comportamento na rota ativa:
+  // É NULL quando adicionamos uma nova cidade,
+  // e NÃO NULL quando estamos editando uma existente.
+  id?: number;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -39,36 +44,59 @@ export class CityEditComponent {
 
   loadData() {
     // recupera o ID a partir do parametro id
-    var id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.id = +this.activatedRoute.snapshot.paramMap.get('id');
 
-    // buscar a cidade no servidor
-    var url = this.baseUrl + "api/cities/" + id;
-    this.http.get<City>(url).subscribe(result => {
-      this.city = result;
-      this.title = "Edição - " + this.city.name;
+    if (this.id) {
+      // Modo edição, buscar cidade no servidor
+      var url = this.baseUrl + "api/cities/" + this.id;
+      this.http.get<City>(url).subscribe(result => {
+        this.city = result;
+        this.title = "Edição - " + this.city.name;
 
-      // atualizar o form com o valor da cidade
-      this.form.patchValue(this.city);
-    }, error => console.error(error));
+        // atualizar o form com o valor da cidade
+        this.form.patchValue(this.city);
+      }, error => console.error(error));
+    }
+    else {
+      // Modo Adição
+      this.title = "Criar nova Cidade";
+    }
   }
 
+
   onSubmit() {
-    var city = this.city;
+
+    var city = (this.id) ? this.city : <City>{};
 
     city.name = this.form.get("name").value;
-    city.lat = this.form.get("lat").value;
-    city.lon = this.form.get("lon").value;
+    city.lat = +this.form.get("lat").value;
+    city.lon = +this.form.get("lon").value;
 
-    console.log(city.name, city.lat, city.lon);
+    if (this.id) {
+      // EDIÇÃO
+      var url = this.baseUrl + "api/cities/" + this.city.id;
+      this.http
+        .put<City>(url, city)
+        .subscribe(result => {
 
-    var url = this.baseUrl + "api/cities/" + this.city.id;
-    this.http
-      .put<City>(url, city)
-      .subscribe(resutl => {
-        console.log("Cidade Id: [" + city.id + "] foi salvo.")
+          console.log("City " + city.id + " foi atualizado.");
 
-        // voltar para a visualização das cidades
-        this.router.navigate(['/cities']);
-      }, error => console.log(error));
+          // voltar para view Cidades
+          this.router.navigate(['/cities']);
+        }, error => console.log(error));
+    }
+    else {
+      // ADIÇÃO
+      var url = this.baseUrl + "api/cities";
+      this.http
+        .post<City>(url, city)
+        .subscribe(result => {
+
+          console.log("Cidade " + result.id + " foi criada.");
+
+            // voltar para view Cidades
+            this.router.navigate(['/cities']);
+        }, error => console.log(error));
+    }
   }
 }
