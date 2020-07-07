4927c39cfa3eb2dd1c72d859f9a987fbb9c7708f
Marcos Jose de Jesus <using.rpa.projects@outlook.com>
2020-07-07 00:59:36 -0300

    Adicionado validação de tela cidade

--- ClientApp/src/app/cities/city-edit.component.ts
+++ ClientApp/src/app/cities/city-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, AsyncValidatorFn } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { City } from './City';
 import { Country } from './../countries/Country';
@@ -42,7 +44,7 @@ export class CityEditComponent {
       lat: new FormControl('', Validators.required),
       lon: new FormControl('', Validators.required),
       countryId: new FormControl('', Validators.required)
-    });
+    }, null, this.isDupeCity());
 
     this.loadData();
   }
@@ -83,7 +85,6 @@ export class CityEditComponent {
     }, error => console.error(error));
   }
 
-
   onSubmit() {
 
     var city = (this.id) ? this.city : <City>{};
@@ -118,6 +119,23 @@ export class CityEditComponent {
           // voltar para view Cidades
           this.router.navigate(['/cities']);
         }, error => console.log(error));
+    }
+  }
+
+  isDupeCity(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<{ [key: string]: any } | null> => {
+
+      var city = <City>{};
+      city.id = (this.id) ? this.id : 0;
+      city.name = this.form.get("name").value;
+      city.lat = +this.form.get("lat").value;
+      city.lon = +this.form.get("lon").value;
+      city.countryId = +this.form.get("countryId").value;
+
+      var url = this.baseUrl + "api/cities/IsDupeCity";
+      return this.http.post<boolean>(url, city).pipe(map(result => {
+        return (result ? { isDupeCity: true } : null);
+      }));
     }
   }
 }
