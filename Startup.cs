de59c46ebd35eb38d3459f0bb86cf56ef04351d0
Marcos Jose de Jesus <using.rpa.projects@outlook.com>
2020-10-21 00:04:49 -0300

    Implementando a regras para usuários

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using WorldCities.Data;
+using WorldCities.Data.Models;
 
 namespace WorldCities
 {
@@ -25,24 +27,41 @@ namespace WorldCities
 		{
 
 			services.AddControllersWithViews()
-				.AddJsonOptions(options => {
-					 // set this option to TRUE to indent the JSON output
-					 options.JsonSerializerOptions.WriteIndented = true;
-					 // set this option to NULL to use PascalCase instead of camelCase (default)
-					 // options.JsonSerializerOptions.PropertyNamingPolicy = null;
-				 });
+				.AddJsonOptions(options =>
+				{
+					// set this option to TRUE to indent the JSON output
+					options.JsonSerializerOptions.WriteIndented = true;
+					// set this option to NULL to use PascalCase instead of camelCase (default)
+					// options.JsonSerializerOptions.PropertyNamingPolicy = null;
+				});
 
-				 // In production, the Angular files will be served from this directory
-				 services.AddSpaStaticFiles(configuration =>
-			{
-				configuration.RootPath = "ClientApp/dist";
-			});
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+	   {
+		   configuration.RootPath = "ClientApp/dist";
+	   });
 
 			// Add EntityFramework support for SqlServer
 			//services.AddEntityFrameworkSqlServer();
 
 			// Add ApplicationDbContext.
 			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			//Add ASP.NET Core Identity support
+			services.AddDefaultIdentity<ApplicationUser>(options =>
+			{
+				options.SignIn.RequireConfirmedAccount = true;
+				options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequiredLength = 8;
+			})
+			.AddRoles<IdentityRole>()
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			services.AddIdentityServer().AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+			services.AddAuthentication().AddIdentityServerJwt();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -85,6 +104,10 @@ namespace WorldCities
 			}
 
 			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseIdentityServer();
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
